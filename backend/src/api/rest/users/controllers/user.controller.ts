5bc307aeef446dd77fc2f04399b966c7344441be
Thanaphum Thepwan <thanaphum.tw@gmail.com>
2023-09-17 16:15:28 +0700

    feat: auth

--- backend/src/api/rest/users/controllers/user.controller.ts
+++ backend/src/api/rest/users/controllers/user.controller.ts
@@ -1,5 +1,6 @@
-import { RequestHandler } from 'express';
-import User from '../models/user.model';
+import { RequestHandler, Request, Response } from "express";
+import User, { IUser } from "../models/user.model";
+import { RequestCustom } from "../middleware/user.middleware";
 
 const getAllUsers: RequestHandler = async (req, res) => {
   try {
@@ -9,7 +10,137 @@ const getAllUsers: RequestHandler = async (req, res) => {
     res.status(400).json({});
   }
 };
+const sendTokenResponse = (user: IUser, statusCode: number, res: Response) => {
+  const token = user.getSignedJwtToken();
+  const options = {
+    expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    _id: user._id,
+    name: user.userName,
+    email: user.email,
+    token,
+  });
+};
+
+const register: RequestHandler = async (req, res) => {
+  try {
+    const { email, userName, firstName, lastName, role, password } = req.body;
+    console.log(req.body);
+    const user = await User.create({
+      email,
+      userName,
+      firstName,
+      lastName,
+      role,
+      password,
+    });
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+    console.error(err);
+  }
+};
+const login: RequestHandler = async (req, res) => {
+  try {
+    const { input, password } = req.body;
+    if (!input || !password) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          msg: "Please provide an email or username and password",
+        });
+    }
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const isEmail = emailPattern.test(input);
+    var user: IUser;
+    if (isEmail) {
+      user = await User.findOne({ email: input }).select("+password");
+    } else {
+      user = await User.findOne({ userName: input }).select("+password");
+    }
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: "Invalid credentials",
+      });
+    }
+    const isMatch = await user.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        msg: "Invalid credentials",
+      });
+    }
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      msg: "Cannot convert email or password to string",
+    });
+  }
+};
+
+const getMe: RequestHandler = async (expressReq, res) => {
+  const req = expressReq as RequestCustom
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+};
+
+
+const logout: RequestHandler = async (req,res) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+};
+
+const updateUser: RequestHandler = async (req,res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+}
+
+const deleteUser: RequestHandler = async (req,res) => {
+  try {
+    const user = await User.findByIdAndRemove(req.params.id);
+    if (!user) {
+      res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: {} });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
+}
 
 export const UserController = {
   getAllUsers,
+  register,
+  login,
+  getMe,
+  logout,
+  updateUser,
+  deleteUser
 };
--- backend/src/api/rest/users/route.ts
+++ backend/src/api/rest/users/route.ts
@@ -1,9 +1,16 @@
-import { Router } from 'express';
-import { UserController } from './controllers/user.controller';
-
+import { Router } from "express";
+import { UserController } from "./controllers/user.controller";
+import { UserMiddleware } from "./middleware/user.middleware";
 const userRouter = Router();
 
 //get all users
-userRouter.get('/', UserController.getAllUsers);
-
+userRouter.get("/", UserController.getAllUsers);
+userRouter
+  .put("/:id", UserMiddleware.protect, UserController.updateUser)
+  .delete("/:id", UserMiddleware.protect, UserController.deleteUser);
+userRouter
+  .post("/auth/register", UserController.register)
+  .post("/auth/login", UserController.login)
+  .get("/auth/me", UserMiddleware.protect, UserController.getMe)
+  .get("/auth/logout", UserController.logout);
 export default userRouter;
