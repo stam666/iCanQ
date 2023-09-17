fd577ef5abc201ca1880e0edceceecf90629f74a
Yaninchaya Unjitwattana <yaninchaya152@hotmail.com>
2023-09-17 16:49:58 +0700

    Fix bug: fix grpc establish connection

--- backend/src/api/gateway/index.ts
+++ backend/src/api/gateway/index.ts
@@ -1,11 +1,11 @@
-import express from 'express';
-import { Server } from 'http';
-import dotenv from 'dotenv';
-import { AddressInfo } from 'net';
-import mongoose from 'mongoose';
-import userRouter from '../rest/users/route';
+import express from "express";
+import { Server } from "http";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import mongoose from "mongoose";
+import userRouter from "../rest/users/route";
+//import client from "./grpc-cilent/order.client";
 import client from "./grpc-cilent/order.client";
-// const client = require("./grpc-cilent/order.client");
 
 dotenv.config();
 
@@ -17,26 +17,28 @@ const startGateway = async (): Promise<AddressInfo> => {
   app.use(express.urlencoded({ extended: true }));
 
   const mongoUrl =
-    process.env.MONGODB_URL || 'mongodb://localhost:27017/testDB';
+    process.env.MONGODB_URL || "mongodb://localhost:27017/testDB";
   mongoose.connect(mongoUrl);
 
+  console.log("Connected to MongoDB");
   app.use((req, res, next) => {
     // maybe authenticate in gateway
     next();
   });
 
   // mock proxy userService
-  app.use('/users', userRouter);
-	// test gRPC
-	app.get("/", (req, res) => {
-		client.getAllOrder(null, (err: any, data: any) => {
-			if (!err) {
-				res.render("menu", {
-					results: data.menu
-				});
-			}
-		});
-	});
+  app.use("/users", userRouter);
+  // test gRPC
+  app.get("/", (req, res) => {
+    client.getAllOrder(null, (err: any, data: any) => {
+      if (!err) {
+        console.log(data);
+        res.send(data);
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
   const port = process.env.PORT || 8080;
   connection = app.listen(port, () => {});
--- backend/src/api/grpc/orders/server.ts
+++ backend/src/api/grpc/orders/server.ts
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-const  Order = require("./models/order.model");
-// import path from "../../../config/protos/order.proto"
+const mongoose = require("mongoose");
+import Order from "./models/order.model";
 const PROTO_PATH = "../../../config/protos/order.proto";
+//import { OrderList } from "/Users/yaninchaya/Desktop/SoftwareArch/iCanQ/backend/src/config/protos/order.proto";
 
-require("dotenv").config({ path: '.env' });
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/testDB');
-
-var grpc = require("grpc");
+require("dotenv").config({ path: ".env" });
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/testDB");
+console.log("Connected to MongoDB");
+var grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
 
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-	keepCase: true,
-	longs: String,
-	enums: String,
-	arrays: true
+  keepCase: true,
+  longs: String,
+  enums: String,
+  arrays: true,
 });
 
 var orderProto = grpc.loadPackageDefinition(packageDefinition);
@@ -22,53 +22,60 @@ var orderProto = grpc.loadPackageDefinition(packageDefinition);
 const server = new grpc.Server();
 
 server.addService(orderProto.OrderService.service, {
-	getAll: async (call: any, callback: any) => {
-		const menu = await Order.find();
-		callback(null, { menu });
-	},
-	get: async (call: any, callback: any) => {
-		const order = await Order.findById(call.request.id);
+  getAllOrder: async (_: any, callback: any) => {
+    const orders = await Order.find();
 
-		if (order) {
-			callback(null, order);
-		} else {
-			callback({
-				code: grpc.status.NOT_FOUND,
-				details: "Not found"
-			});
-		}
-	},
-	insert: async (call: any, callback: any) => {
-		const order = await Order.create(call.request);
-		callback(null, order);
-	},
-	update: async (call: any, callback: any) => {
-		const { id, ...body } = call.request;
-		const order = await Order.findByIdAndUpdate(id, body, { new: true });
+    callback(null, { orders });
+  },
+  //   get: async (call: any, callback: any) => {
+  //     const order = await Order.findById(call.request.id);
 
-		if (order) {
-			callback(null, order);
-		} else {
-			callback({
-				code: grpc.status.NOT_FOUND,
-				details: "Not Found"
-			});
-		}
-	},
-	remove: async (call: any, callback: any) => {
-		const order = await Order.findByIdAndDelete(call.request.id);
+  //     if (order) {
+  //       callback(null, order);
+  //     } else {
+  //       callback({
+  //         code: grpc.status.NOT_FOUND,
+  //         details: "Not found",
+  //       });
+  //     }
+  //   },
+  //   insert: async (call: any, callback: any) => {
+  //     const order = await Order.create(call.request);
+  //     callback(null, order);
+  //   },
+  //   update: async (call: any, callback: any) => {
+  //     const { id, ...body } = call.request;
+  //     const order = await Order.findByIdAndUpdate(id, body, { new: true });
 
-		if (order) {
-			callback(null, {});
-		} else {
-			callback({
-				code: grpc.status.NOT_FOUND,
-				details: "NOT Found"
-			});
-		}
-	}
+  //     if (order) {
+  //       callback(null, order);
+  //     } else {
+  //       callback({
+  //         code: grpc.status.NOT_FOUND,
+  //         details: "Not Found",
+  //       });
+  //     }
+  //   },
+  //   remove: async (call: any, callback: any) => {
+  //     const order = await Order.findByIdAndDelete(call.request.id);
+
+  //     if (order) {
+  //       callback(null, {});
+  //     } else {
+  //       callback({
+  //         code: grpc.status.NOT_FOUND,
+  //         details: "NOT Found",
+  //       });
+  //     }
+  //   },
 });
+console.log(`Listening on localhost:30043`);
+server.bindAsync(
+  "127.0.0.1:30043",
+  grpc.ServerCredentials.createInsecure(),
+  () => {
+    server.start();
 
-server.bind("127.0.0.1:30043", grpc.ServerCredentials.createInsecure());
-console.log("Server running at http://127.0.0.1:30043");
-server.start();
+    console.log("server is running on 127.0.0.1:30043");
+  }
+);
