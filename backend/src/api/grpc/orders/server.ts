d32383521db345a197dedf0d09fe33fd5f7be2ec
stam <stamp.chayakorn@gmail.com>
2023-10-17 22:54:26 +0700

    add REST order service (controller + grpc client)

--- backend/src/api/grpc/orders/server.ts
+++ backend/src/api/grpc/orders/server.ts
@@ -1,18 +1,11 @@
 const mongoose = require("mongoose");
-import Order from "./models/order.model";
-import { ISingleOrderRequest, IOrderItem, IOrderList } from "./orderTypes";
-import { ServerUnaryCall, sendUnaryData } from "@grpc/grpc-js";
-const PROTO_PATH = "../../../config/protos/order.proto";
+import { OrderController } from "./controllers/order.controller";
+const PROTO_PATH = "./src/shared/proto/order.proto";
 
 // Change dotenv path
 require("dotenv").config({
-  path: "../../../../config.env",
+  path: "./config.env",
 });
-
-// Connect to MongoDB
-mongoose.set("strictQuery", true);
-mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/testDB");
-console.log("Connected to MongoDB on " + process.env.MONGO_URL);
 
 // Create gRPC server
 var grpc = require("@grpc/grpc-js");
@@ -31,153 +24,13 @@ const server = new grpc.Server();
 
 // Add service to gRPC server
 server.addService(orderProto.OrderService.service, {
-  // GET All Order -> This service needs to be fixed.
-  // getAllOrder: async (_: any, callback: any) => {
-  //   const orders = await Order.find();
-
-  //   const response = orders.map((order) => ({
-  //     userId: order.userId,
-  //     restaurantId: order.restaurantId,
-  //     queueNumber: order.queueNumber,
-  //     orderLines: order.orderLines,
-  //     orderStatus: order.orderStatus,
-  //     totalPrice: order.totalPrice,
-  //   }));
-  //   console.log(response);
-  //   callback(null, { response });
-  // },
-  getAllOrder: async (_: any, callback: sendUnaryData<IOrderList>) => {
-    try {
-      const orderDocs = await Order.find();
-      const orders = orderDocs.map((order) => ({
-        ...order.toObject(),
-        orderId: order._id.toString(),
-        createdTime: order.createdTime,
-        pickupTime: order.pickupTime,
-      }));
-      callback(null, { orders });
-    } catch (error) {
-      callback({
-        code: grpc.status.INTERNAL,
-        details: "Internal Server Error",
-      });
-    }
-  },
-  // GET One Order
-  get: async (
-    call: ServerUnaryCall<ISingleOrderRequest, IOrderItem>,
-    callback: sendUnaryData<IOrderItem>
-  ) => {
-    const order = await Order.findById(
-      new mongoose.Types.ObjectId(call.request.orderId)
-    );
-
-    if (order) {
-      callback(null, {
-        ...order.toObject(),
-        orderId: order._id.toString(),
-        createdTime: order.createdTime,
-        pickupTime: order.pickupTime,
-      });
-    } else {
-      callback({
-        code: grpc.status.NOT_FOUND,
-        details: "Not found",
-      });
-    }
-  },
-
-  // Create Order
-  insert: async (
-    call: ServerUnaryCall<IOrderItem, IOrderItem>,
-    callback: sendUnaryData<IOrderItem>
-  ) => {
-    const now = new Date();
-    const newOrderItem = new Order({
-      userId: call.request.userId,
-      restaurantId: call.request.restaurantId,
-      createdTime: now,
-      pickupTime: now,
-      queueNumber: call.request.queueNumber,
-      orderLines: call.request.orderLines,
-      orderStatus: call.request.orderStatus,
-      totalPrice: call.request.totalPrice,
-    });
-    try {
-      const result = await newOrderItem.save();
-      console.log(result);
-      callback(null, { ...call.request, orderId: result._id.toString() });
-    } catch (err) {
-      callback({
-        code: grpc.status.INTERNAL,
-        details: "Insert Error",
-      });
-    }
-  },
-
-  // Update an order
-  update: async (
-    call: ServerUnaryCall<IOrderItem, IOrderItem>,
-    callback: sendUnaryData<IOrderItem>
-  ) => {
-    const orderId = call.request.orderId;
-    const updatedData = call.request;
-
-    try {
-      const updatedOrder = await Order.findByIdAndUpdate(
-        orderId,
-        { $set: updatedData },
-        { new: true }
-      );
-
-      if (updatedOrder) {
-        // Convert updatedOrder to IOrderItem format
-        const updatedOrderItem: IOrderItem = {
-          orderId: updatedOrder._id.toString(),
-          userId: updatedOrder.userId,
-          restaurantId: updatedOrder.restaurantId,
-          queueNumber: updatedOrder.queueNumber,
-          orderLines: updatedOrder.orderLines as any, // Type assertion to map format
-          orderStatus: updatedOrder.orderStatus,
-          totalPrice: updatedOrder.totalPrice,
-          createdTime: updatedOrder.createdTime,
-          pickupTime: updatedOrder.pickupTime,
-        };
-
-        callback(null, updatedOrderItem);
-      } else {
-        callback({
-          code: grpc.status.NOT_FOUND,
-          details: "Not found",
-        });
-      }
-    } catch (error) {
-      callback({
-        code: grpc.status.INTERNAL,
-        details: "Internal Server Error",
-      });
-    }
-  },
-
-  // Remove an order
-  remove: async (
-    call: ServerUnaryCall<ISingleOrderRequest, any>,
-    callback: sendUnaryData<any>
-  ) => {
-    const existingOrderItem = await Order.findById(
-      new mongoose.Types.ObjectId(call.request.orderId)
-    );
-    if (existingOrderItem) {
-      await existingOrderItem.deleteOne();
-      callback(null, {});
-    } else {
-      callback({
-        code: grpc.status.NOT_FOUND,
-        details: "Not found",
-      });
-    }
-  },
+  getAllOrder: OrderController.getAllOrder,
+  get: OrderController.get,
+  insert: OrderController.insert,
+  update: OrderController.update,
+  remove: OrderController.remove,
 });
+
 console.log(`Listening on localhost:30043`);
 server.bindAsync(
   "127.0.0.1:30043",
---
+++ backend/src/api/rest/orders/order.controller.ts
@@ -0,0 +1,128 @@
+import { RequestHandler } from "express";
+import axios from "axios";
+import client from "./order.client";
+import { ServerErrorResponse } from "@grpc/grpc-js";
+import { IOrderItem } from "../../../shared/common/interfaces/orderTypes";
+import { RequestCustom } from "../users/middleware/user.middleware";
+
+const getOrders: RequestHandler = async (req, res) => {
+  client.getAllOrder(null, (err: ServerErrorResponse, data: IOrderItem[]) => {
+    if (!err) {
+      console.log(data);
+      res.send(data);
+    } else {
+      console.log(err);
+    }
+  });
+};
+
+const getOrder: RequestHandler = async (req, res) => {
+  const orderId = req.params.id;
+
+  client.get({ orderId }, (err: ServerErrorResponse, data: IOrderItem) => {
+    if (!err) {
+      console.log(data);
+      res.send(data);
+    } else {
+      if (err.details === "Not found") {
+        res.status(404).send("Order Not Found : " + orderId);
+      } else {
+        res.status(500).send("Failed to get Order : " + orderId);
+      }
+    }
+  });
+};
+
+const addOrder: RequestHandler = async (req: RequestCustom, res) => {
+  const userId = req.user.id;
+  const orderDetails = req.body as IOrderItem;
+
+  const totalPrice = req.body.orderLines.reduce(
+    (total: number, orderLine: Map<string, number>) => {
+      return (
+        total +
+        orderLine.get("quantity")! * orderLine.get("price")! * (1 - 0.1)
+      );
+    },
+    0
+  );
+
+  let newOrderItem = {
+    userId,
+    restaurantId: req.body.restaurantId,
+    queueNumber: req.body.queueNumber,
+    orderLines: req.body.orderLines,
+    orderStatus: "Pending",
+    totalPrice,
+  } as IOrderItem;
+
+  client.insert(newOrderItem, (err: ServerErrorResponse, data: IOrderItem) => {
+    if (!err) {
+      console.log("New Order created successfully", data.orderId);
+      res.send("New Order created successfully : " + data.orderId);
+    } else {
+      if (err.details === "Insert error") {
+        res.status(500).send("Failed to create new Order");
+      }
+    }
+  });
+};
+
+const updateOrder: RequestHandler = async (req, res) => {
+  const orderId = req.params.id;
+
+  // Create an object with the updated order data based on the request body
+  const updatedOrderItem = {
+    orderId,
+    userId: req.body.userId,
+    restaurantId: req.body.restaurantId,
+    queueNumber: req.body.queueNumber,
+    orderLines: req.body.orderLines,
+    orderStatus: req.body.orderStatus,
+    totalPrice: req.body.totalPrice,
+  };
+
+  console.log(updatedOrderItem);
+
+  client.update(
+    updatedOrderItem,
+    (err: ServerErrorResponse, data: IOrderItem) => {
+      if (!err) {
+        console.log("Order updated successfully", data.orderId);
+        res.send("Order updated successfully : " + data.orderId);
+      } else {
+        if (err.details === "Not found") {
+          res.status(404).send("Order Not Found : " + orderId);
+        } else {
+          res.status(500).send("Failed to update Order : " + orderId);
+        }
+      }
+    }
+  );
+};
+
+const removeOrder: RequestHandler = async (req, res) => {
+  const orderId = req.params.id;
+
+  client.remove({ orderId }, (err: ServerErrorResponse, _: any) => {
+    if (!err) {
+      console.log("Order removed successfully", orderId);
+      res.send("Order Delete Succesfully");
+    } else {
+      if (err.details === "Not found") {
+        res.status(404).send("Order Not Found : " + orderId);
+      } else {
+        res.status(500).send("Failed to remove Order");
+        console.log(err);
+      }
+    }
+  });
+};
+
+export const OrderController = {
+  getOrders,
+  getOrder,
+  addOrder,
+  updateOrder,
+  removeOrder,
+};
