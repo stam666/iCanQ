20afcdfd7305f422d463564395f9173ca7aa9b1b
stam <stamp.chayakorn@gmail.com>
2023-11-10 14:25:21 +0700

    placeOrder completed

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/", "/myrestaurant"],
+  matcher: ["/", "/myrestaurant", "/order/[orderId]", "/restaurant/[restaurantId]"],
 };
--- client/src/models/order.model.tsx
+++ client/src/models/order.model.tsx
@@ -1,7 +1,47 @@
-export interface IOrder {
-  id: string;
+
+
+export interface IOrderId {
+  orderId: string;
+}
+
+export enum Queue {
+  CREATE = "order.create",
+  UPDATE = "order.update",
+}
+
+export enum OrderStatus {
+  Pending = "pending",
+  Preparing = "preparing",
+  Ready = "ready",
+  Completed = "completed",
+  Cancelled = "cancelled",
+}
+
+export interface IOrderFilter {
+  userId?: string;
+  restaurantId?: string;
+}
+
+export interface IOrderList {
+  orders: IOrderItem[];
+}
+
+export interface IOrderItem {
+  menuId: string;
   name: string;
+  price: number;
   amount: number;
   note: string;
-  totalPrice: string;
+}
+
+export interface IOrder {
+  _id?: string;
+  userId?: string;
+  restaurantId?: string;
+  orderItems?: IOrderItem[];
+  status?: OrderStatus;
+  totalPrice?: number;
+  pickupTime?: Date;
+  createdTime?: Date;
+  updatedTime?: Date;
 }
