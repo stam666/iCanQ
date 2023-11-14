52c0716fa0ec4a36b062a28459167a9c30ef6c39
Gift <giftnichakul@gmail.com>
2023-11-14 10:48:17 +0700

    add k8s

---
+++ kubernetes/delete.sh
@@ -0,0 +1,7 @@
+kubectl delete -f rabbitmq.yaml
+kubectl delete -f user-service.yaml
+kubectl delete -f restaurant-service.yaml
+kubectl delete -f menu-service.yaml
+kubectl delete -f order-service.yaml
+kubectl delete -f review-service.yaml
+kubectl delete -f gateway-service.yaml
---
+++ kubernetes/start.sh
@@ -0,0 +1,7 @@
+kubectl apply -f rabbitmq.yaml
+kubectl apply -f user-service.yaml
+kubectl apply -f restaurant-service.yaml
+kubectl apply -f menu-service.yaml
+kubectl apply -f order-service.yaml
+kubectl apply -f review-service.yaml
+kubectl apply -f gateway-service.yaml
